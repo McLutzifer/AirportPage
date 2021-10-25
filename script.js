87f55af8fc084c87e51d7b07e68038be7ed046d7
McLutzifer <lukasschumi@gmx.at>
2021-10-25 14:18:27 +0200

    copy paste

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const buttonClick = () => {
+    document.getElementById('mainHeading').innerHTML = "Nice, you broke it!";
+    document.getElementById('button').style.display = "none";
+    document.getElementById('menu').style.display = "none";
+}
